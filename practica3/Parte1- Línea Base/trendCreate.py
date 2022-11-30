fecd6571efb0971f279e2cc66daace0083e3836c
Coria05 <94867910+Corix5@users.noreply.github.com>
2022-11-30 17:02:38 -0600

    Add files via upload

---
+++ practica3/Parte1- Línea Base/trendCreate.py
@@ -0,0 +1,12 @@
+import rrdtool
+
+ret = rrdtool.create("../RRD/trend.rrd",
+                        "--start",'N',
+                        "--step",'60',
+                        "DS:CPUload:GAUGE:60:0:100",
+                        "RRA:AVERAGE:0.5:1:24")
+if ret:
+    print (rrdtool.error())
+
+
+# C:\Users\luisd\OneDrive\Documents\Servicios de red\practicas\Introduccion_SNMP\6-AdministraciónDeRendimiento\RRD
